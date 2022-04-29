5cb03e24aec75eed3e770d8cb0e9d5893fc25e51
abcdefghilklmnop <85159355+abcdefghilklmnop@users.noreply.github.com>
2022-04-29 09:20:43 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,159 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 5 5 5 5 5 5 5 . . . . . . . 
+        . 5 3 3 3 3 3 3 5 . . . . . . . 
+        5 3 3 3 3 3 3 3 5 . . . . . . . 
+        . 5 3 3 3 3 3 3 5 . . . . . . . 
+        . . 5 5 5 5 5 5 5 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, Simmonato, -100, 0)
+    animation.runImageAnimation(
+    Simmonato,
+    [img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . c d f d d f d e e f f . . . . 
+        . c d f d d f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . . . 
+        c c c c c d d e e e f c . . . . 
+        . f d d d d e e e f f . . . . . 
+        . . f f f f f e e e e f . . . . 
+        . . . . f f e e e e e e f . f f 
+        . . . f e e f e e f e e f . e f 
+        . . f e e f e e f e e e f . e f 
+        . f b d f d b f b b f e f f e f 
+        . f d d f d d f d d b e f f f f 
+        . . f f f f f f f f f f f f f . 
+        `,img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . c d f d d f d e e f f . . . . 
+        . c d f d d f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . . . 
+        c c c c c d d e e e f c . . . . 
+        . f d d d d e e e f f . . . . . 
+        . . f e e e f f e e e f . . . . 
+        . . f f f f f e e e e e f . f f 
+        . . f d b f e e f f e e f . e f 
+        . f f d d f e f f e e e f . e f 
+        . f f f f f f e b b f e f f e f 
+        . f d d f e e e d d b e f f f f 
+        . . f f f f f f f f f f f f f . 
+        `,img`
+        . . . . . f f f f f . . . . . . 
+        . . . . f e e e e e f . . . . . 
+        . . . f d d d d d e e f . . . . 
+        . . f f f d d f f d e f f . . . 
+        . c d d e e d d d d e d d f . . 
+        . c c d d d d c d d e d f f f . 
+        . c d c c c c d d d e d f b d f 
+        . . c d d d d d d e e f f d d f 
+        . . . c d d d d e e f f e f f f 
+        . . . . f f f e e f e e e f . . 
+        . . . . f e e e e e e e f f f . 
+        . . . f e e e e e e f f f e f . 
+        . . f f e e e e f f f f f e f . 
+        . f b d f e e f b b f f f e f . 
+        . f d d f e e f d d b f f f f . 
+        . f f f f f f f f f f f f f . . 
+        `,img`
+        . . . . . f f f f f . . . . . . 
+        . . . . f e e e e e f . . . . . 
+        . . . f d d d d d d e f . . . . 
+        . . f d f f d d f f d f f . . . 
+        . c d d d e e d d d d e d f . . 
+        . c d c d d d d c d d e f f . . 
+        . c d d c c c c d d d e f f f f 
+        . . c d d d d d d d e f f b d f 
+        . . . c d d d d e e f f f d d f 
+        . . . . f f f e e f e e e f f f 
+        . . . . f e e e e e e e f f f . 
+        . . . f e e e e e e f f f e f . 
+        . . f f e e e e f f f f f e f . 
+        . f b d f e e f b b f f f e f . 
+        . f d d f f f f d d b f f f f . 
+        . f f f f f f f f f f f f f . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . . f d d d d d e e f f . . . . 
+        . c d d d f f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c f f d d c d d e e b d c . . . 
+        f d d f e f f f e e e f . . . . 
+        f d d f e e e f f f f f . . . . 
+        f f f f f e e e e e f f . f f . 
+        . f f f e f f e e e f f . e f . 
+        . f b d f e f f b b f f f e f . 
+        . f d d f e e f d d b f f e f . 
+        . f f f f f f f f f f f f f . . 
+        `,img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . . f d d f d d e e f f . . . . 
+        . c d d d f d d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . . . 
+        c f f f f d d d e e f c f . . . 
+        . f b d f f f e e e e f . . . . 
+        . f d d f e f f f e e f . . . . 
+        . . f f f e e e e f f f . f f . 
+        . . f e e f e e e e f f . e f . 
+        . f f e e e f f f f f f f e f . 
+        . f b d f e e f b b f f f e f . 
+        . f d d f f e e d d b f f f f . 
+        . f f f f f f f f f f f f f . . 
+        `],
+    100,
+    true
+    )
+})
+let ben: Sprite = null
+let projectile: Sprite = null
+let Simmonato: Sprite = null
+scene.setBackgroundColor(0)
+scene.setBackgroundImage(assets.image`cityscape2`)
+Simmonato = sprites.create(assets.image`Tommy`, SpriteKind.Player)
+Simmonato.setPosition(155, 65)
+controller.moveSprite(Simmonato, 100, 100)
+Simmonato.setStayInScreen(true)
+game.onUpdateInterval(500, function () {
+    ben = sprites.create(img`
+        . . . . f f f f f f f . . . . . 
+        . . . f f 5 5 5 5 5 f f . . . . 
+        . . f f 5 5 5 5 5 5 5 f f . . . 
+        . f f 5 5 f 5 5 5 f 5 5 f f . . 
+        . f 5 5 5 f 5 5 5 f 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 f 5 5 5 5 5 f 5 5 f . . 
+        . f f 5 5 f f f f f 5 5 f f . . 
+        . . f f 5 5 5 5 5 5 5 f f . . . 
+        . . . f f 5 5 5 5 5 f f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    ben.setVelocity(50, 0)
+    ben.setPosition(0, randint(20, 120))
+})
